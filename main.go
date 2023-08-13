056a4012fd2a39bd0ac5b69879fd3d1f493605ca
Mike Spinks <mikespinks@gmail.com>
2023-08-13 19:45:10 +0100

    feat: update update Dockerfile update docs add writeHeader and environment variable ports

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func video(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+		return
+	}
 	w.Header().Set("Content-Type", "video/mp4")
 	http.ServeFile(w, r, "video/SampleVideo_1280x720_1mb.mp4")
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	fmt.Println(port)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/video", video)
 
-	log.Println("Video streaming on :3000")
-	err := http.ListenAndServe(":3000", mux)
+	log.Println("Video streaming on :" + port)
+	err := http.ListenAndServe(":"+port, mux)
 	log.Fatal(err)
 }
