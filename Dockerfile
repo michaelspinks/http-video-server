18914d67b89e71009b3d23c16d2e6c939b6589bd
Mike Spinks <mikespinks@gmail.com>
2023-08-16 22:19:58 +0100

    feat: update server Server now forwards http request to video-storage microservice

--- Dockerfile
+++ Dockerfile
@@ -14,16 +14,6 @@ RUN go build -o http-video-server
 
 FROM debian:bullseye-slim
 
-# Optional:
-# To bind to a TCP port, runtime parameters must be supplied to the docker command.
-# But we can document in the Dockerfile what ports
-# the application is going to listen on by default.
-# https://docs.docker.com/engine/reference/builder/#expose
-EXPOSE 3000
-
-# Set the PORT environment variable
-ENV PORT=3000
-
 WORKDIR /
 COPY --from=build-env /app/http-video-server /http-video-server
 
--- main.go
+++ main.go
@@ -2,28 +2,62 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
 func video(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(405)
-		w.Write([]byte("Method not allowed"))
+
+	log.Println("Checking environment variables...")
+
+	// Retrieve environment variables
+	VIDEO_STORAGE_HOST, ok1 := os.LookupEnv("VIDEO_STORAGE_HOST")
+	VIDEO_STORAGE_PORT, ok2 := os.LookupEnv("VIDEO_STORAGE_PORT")
+
+	// Ensure environment variables are set
+	if !ok1 || !ok2 {
+		http.Error(w, "Environment variables not set", http.StatusInternalServerError)
+		log.Println("Environment variables not set!")
 		return
 	}
-	w.Header().Set("Content-Type", "video/mp4")
-	http.ServeFile(w, r, "video/SampleVideo_1280x720_1mb.mp4")
+
+	serviceURL := fmt.Sprintf("http://%s:%s/video?path=SampleVideo_1280x720_1mb.mp4", VIDEO_STORAGE_HOST, VIDEO_STORAGE_PORT)
+
+	req, err := http.NewRequest(r.Method, serviceURL, r.Body)
+	if err != nil {
+		http.Error(w, "Error creating request", http.StatusInternalServerError)
+		log.Println("Error creating request!")
+		return
+	}
+	req.Header = r.Header
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, "Error forwarding request", http.StatusInternalServerError)
+		log.Println("Error forwarding request!")
+		return
+	}
+	log.Printf("Sending Request to %s", serviceURL)
+	defer resp.Body.Close()
+
+	// Copy the response from the other service to the original response writer
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	fmt.Println(port)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/video", video)
 
-	log.Println("Video streaming on :" + port)
-	err := http.ListenAndServe(":"+port, mux)
+	log.Println("Video streaming on :4001")
+	err := http.ListenAndServe(":80", mux)
 	log.Fatal(err)
 }
